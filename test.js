474c73ca1eb2a6a2711aa0794df8bb7ca08536d9
Shubham Keserwani <shubhamkesar@live.com>
2014-12-06 05:39:42 +0530

    Demo file

---
+++ test.js
@@ -0,0 +1,24 @@
+
+var server = require('tk102');
+
+server.createServer({
+  port: 1337,
+  address: '127.0.0.1',
+  timeout: 1
+});
+
+server.on('track', function (gps) {
+  console.log(gps);
+});
+
+server.on('timeout', function () {
+  console.log("timed out!");
+});
+
+server.on('listening', function() {
+  console.log('started listening!');
+});
+
+server.on('connection', function (socket) {
+  console.log('Connection established with ' + socket.remoteAddress);
+});
